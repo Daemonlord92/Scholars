a43facf88a822048492caf0200ae33b308c1271a
Matthew Jeshua Martin <matthewjeshuamartin@Matthews-MBP.hsd1.va.comcast.net>
2024-06-11 10:33:46 -0400

    Collections redo and bitwise

--- Collection/src/main/java/org/blitmatthew/Main.java
+++ Collection/src/main/java/org/blitmatthew/Main.java
@@ -1,14 +1,23 @@
 package org.blitmatthew;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    public static void generateSubsets(int[] arr, int n){
+        for (int i = 0; i < (1 << n); i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    System.out.println(arr[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*System.out.println("Hello world!");
 
         List<String> names = new ArrayList<>();
         names.add("Nick");
@@ -31,6 +40,59 @@ public class Main {
         System.out.println(set);
         System.out.println(set);
         set.add("Omer");
-        System.out.println(set);
+        System.out.println(set);*/
+
+        List<Integer> numList = new ArrayList<>();
+        numList.add(1); // 0
+        numList.add(26);// 1
+        numList.add(625);// 2
+        numList.add(786);// 3
+        numList.add(26);// 4
+//        numList.add(null);//5
+//        numList.add(null);//6
+        System.out.println(numList);
+        System.out.println(numList.get(3));
+        numList.remove(1);
+        System.out.println(numList);
+        System.out.println(numList.get(1));
+        System.out.println(numList.contains(65));
+        Set<Integer> numSet = new HashSet<>();
+        numSet.add(1);
+        numSet.add(26);
+        numSet.add(625);
+        System.out.println(numSet);
+        numSet.add(26);
+        System.out.println(numSet);
+//        numSet.add(null);
+        System.out.println(numSet);
+        /*
+        * Constant: O(1)
+        * Linear time: O(n)
+        * Logarithmic time: O(n log n)
+        * Quadratic time: O(n^2)
+        * Exponential time: O(2^n)
+        * Factorial time: O(n!)
+        * */
+
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println(i + "i");
+            for (int j = 0; j < 6; j++) {
+                System.out.println(j + "j");
+            }
+        }*/
+
+        int[] arr = { 3, 5, 1, 7, 9, 10, 15};
+        generateSubsets(arr, arr.length);
+
+        //Bits Position
+        // 01
+        // 11101 = 29
+        // <<
+        // 01110 = 14
+        // >>
+        // 11100 = 28
+        // 00011 = 3
+        // 00100
+        // 1000001
     }
 }
