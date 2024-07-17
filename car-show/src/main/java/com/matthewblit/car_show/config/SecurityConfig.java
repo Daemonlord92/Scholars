fadbff0b0ee114245b030ed7f0e6327b0a6d7e32
matthewjeshuamartin <matthew-martin@programmer.net>
2024-07-17 11:58:58 -0400

    Basics of Security

---
+++ car-show/src/main/java/com/matthewblit/car_show/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.matthewblit.car_show.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(c -> c
+                        .requestMatchers(HttpMethod.GET,"/api/v1/car/")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/car/")
+                        .hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("userPass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
